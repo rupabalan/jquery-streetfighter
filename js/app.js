61c4fe841f45b64267d3174fa492751ab465ed1f
rupabalan <psriniboise@gmail.com>
2015-06-09 20:05:24 -0600

    commiting this project changes

--- js/app.js
+++ js/app.js
@@ -1,43 +1,58 @@
-$(document).ready (function()
-{
+$(document).ready (function(){
 	$('.ryu').mouseenter(function(){
-
-     //alert('mouse entered rue div');
      $('.ryu-still').hide();
      $('.ryu-ready').show();
-	}
-	)
-
+	})
   .mouseleave(function(){
-
-    $(".ryu-ready").hide();
     $(".ryu-still").show();
-   }
-   )
-
+    $(".ryu-ready").hide();
+   })
    .mousedown(function(){
-
    	playHadouken();
-
    	$('.ryu-ready').hide();
    	$('.ryu-throwing').show();
    	$('.hadouken').finish().show().animate(
-   		{'left':'1020px'},500,
+   		{'left':'1020px'},
+      500,
    		function(){
    			$(this).hide();
-   			$(this).css('left','520px')
-   		}
-   		);
+   			$(this).css('left','520px');
+   		});	
    })
-
    .mouseup(function(){
     
-    
-    $('ryu-throwing').hide();
-    $('ryu-ready').show();
-
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
    })
+
+
+
+    $(document).keydown(function(event){
+
+
+       if ( event.which== 88 ) {
+     
+
+        $('.ryu-cool').show();
+         $('.ryu-ready').hide();
+         $('.ryu-still').hide();
+       
+    }
+      $(document).keyup(function(event){
+       if ( event.keyCode == 88 ) {
+           $('.ryu-cool').hide();
+           $('.ryu-still').show();
+         
+         }
+     });
+
+
+}); 
+
+      
 });
+
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
